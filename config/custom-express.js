9619753450a0834002a191fbec83238b89e6b359
BrunoSenoni <bruno_senoni@hotmail.com>
2018-11-27 18:34:30 -0200

    aplicado validacoes da entrada

--- config/custom-express.js
+++ config/custom-express.js
@@ -4,6 +4,8 @@ var express = require('express');
 var consign = require('consign');
 //o bodyparser converte o body da requisição para vários formatos
 var bodyParser = require('body-parser');
+//Validação de dados
+var expressValidator = require('express-validator');
 
 module.exports = function() {
     //Armazena de fato o objeto do express
@@ -11,6 +13,8 @@ module.exports = function() {
     app.use(bodyParser.urlencoded({extended: true}));
     //o app agora é capaz de realizar o parse do corpo de uma requisição
     app.use(bodyParser.json());
+    //o app agora é capaz de realizar validações 
+    app.use(expressValidator());
     //Tudo o que for carregado pelo consign deve ser incluido ao objeto do express que está sendo referenciado pela variável app
     consign()
     //Agora o consign informa ao express que deve fazer um scan de todos os arquivos dentro da pasta controllers e persistencia, carregá-los para dentro do seu objeto
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -7,6 +7,19 @@ module.exports = function(app){
  //Function é uma função de Callback   
  //Essa função recebe dados para realizar um pagamento 
     app.post('/pagamentos/pagamento', function(req, res){
+        //.assert é um validator do express. Serve para validação, filtragem e tratamento de dados 
+        //O campo forma_de_pagamento não pode ser vazio
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+        //O campo valor não pode ser vazio e deve ser decimal
+        req.assert("valor", "Valor é obrigatório e deve ser decimal").notEmpty().isFloat();
+
+        var erros = req.validationErros();
+
+        if(erros){
+            console.log("erros de validacao encontrados");
+            res.status(400).send(erros);
+        }
+
     //corpo da requisição
         var pagamento = req.body;
         console.log('processando uma requisição de um novo pagamento');
@@ -18,8 +31,13 @@ module.exports = function(app){
         var pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
         pagamentoDao.salva(pagamento, function(erro, resultado){
+            if(erro){
+                console.log("Erro ao inserir no banco" + erro);
+                res.status(400).send(erro);
+            }else{
            console.log('pagamento criado');
            res.json(pagamento);
+        }
         });
 
         
